f2a98aa5ac9e2145806377b8cc9804dfac7f7146
John Glennan <glennan20@gmail.com>
2020-08-20 21:01:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,8 +25,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[] = index_number
-  puts index_number
+  index_number = array[]
   
 end
 
